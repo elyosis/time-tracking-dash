8191f459e298396dfbdb02b16ebcffa7fffc4c79
Antía <elyosis@proton.me>
2023-12-29 20:36:47 +0100

    Arranged elements in desktop grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import classes from "./App.module.css";
+import UserPanel from "./components/UserPanel";
+import Card from "./components/Card";
 
 function App() {
-
   return (
-    <h1>Hello world</h1>
-  )
+    <div className={classes.container}>
+      <UserPanel />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 }
 
-export default App
+export default App;
