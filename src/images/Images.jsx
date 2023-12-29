9406754fa80e052f5c02eaeabc8dc1c29b359a2c
Antía <elyosis@proton.me>
2023-12-29 19:19:07 +0100

    Fixed icon extensions

--- src/images/Images.jsx
+++ src/images/Images.jsx
@@ -1,12 +1,12 @@
 const IMAGES = {
   user: new URL('./image-jeremy.png', import.meta.url).href,
-  exercise: new URL('./icon-exercise.png', import.meta.url).href,
-  play: new URL('./icon-play.png', import.meta.url).href,
-  selfcare: new URL('./icon-self-care.png', import.meta.url).href,
-  social: new URL('./icon-social.png', import.meta.url).href,
-  study: new URL('./icon-study.png', import.meta.url).href,
-  work: new URL('./icon-work.png', import.meta.url).href,
-  ellipsis: new URL('./icon-ellipsis.png', import.meta.url).href
+  exercise: new URL('./icon-exercise.svg', import.meta.url).href,
+  play: new URL('./icon-play.svg', import.meta.url).href,
+  selfcare: new URL('./icon-self-care.svg', import.meta.url).href,
+  social: new URL('./icon-social.svg', import.meta.url).href,
+  study: new URL('./icon-study.svg', import.meta.url).href,
+  work: new URL('./icon-work.svg', import.meta.url).href,
+  ellipsis: new URL('./icon-ellipsis.svg', import.meta.url).href
 }
 
 export default IMAGES;
